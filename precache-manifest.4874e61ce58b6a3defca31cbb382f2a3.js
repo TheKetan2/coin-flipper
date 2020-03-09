3e2af403f63a1376013fab3b2cd80642390e1622
Ketan <ketantoo@gmail.com>
2020-03-09 17:41:42 +0530

    Updates

---
+++ precache-manifest.4874e61ce58b6a3defca31cbb382f2a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5810b58d60d391533dd189a146b38b7",
+    "url": "/coin-flipper/index.html"
+  },
+  {
+    "revision": "fb77b4cb480853e84b7e",
+    "url": "/coin-flipper/static/css/main.e05ce79a.chunk.css"
+  },
+  {
+    "revision": "3b978d39227b4c184d88",
+    "url": "/coin-flipper/static/js/2.6b22b343.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/coin-flipper/static/js/2.6b22b343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb77b4cb480853e84b7e",
+    "url": "/coin-flipper/static/js/main.177e545a.chunk.js"
+  },
+  {
+    "revision": "708fa9a286b41d72d23e",
+    "url": "/coin-flipper/static/js/runtime-main.c661bda8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coin-flipper/static/media/logo.5d5d9eef.svg"
+  }
+]);
